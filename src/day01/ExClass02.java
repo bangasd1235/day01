9ad6a314a9ac3a5b04fa7fc90173d125297774ad
bang <asd@asd.com>
2024-09-26 14:39:58 +0900

    영수증

---
+++ src/day01/ExClass02.java
@@ -0,0 +1,31 @@
+package day01;
+
+public class ExClass02 {
+	public static void main(String[] args) {
+		System.out.println("난\n녕\n하\n셍\n");
+		System.out.println("1\t1234567\t12345678\t1");
+		System.out.println("이름\t나이");
+		System.out.println("홍머시기\t2000살");
+		System.out.println("\"   \\");
+		System.out.println(100+100);
+		System.out.println(100+100.222);
+		
+		System.out.println("안녕"+"하세요");
+		System.out.println("백"+100);
+		System.out.println("100"+200);
+		
+		System.out.println("안녕"+(100+200));
+		
+		
+		System.out.println("=======================================");
+		System.out.println("이름\t나이\t전화번호\t\t회비");
+		System.out.println("=======================================");
+		System.out.println("홍길동\t\""+15+"\"\t"+3672+"-"+1234+"\t\\"+20000);
+		System.out.println("고길동\t\""+15+"\"\t"+2238+"-"+1234+"\t\\"+30000);
+		System.out.println("김말이\t\""+15+"\"\t"+1234+"-"+1234+"\t\\"+50000);
+		System.out.println("---------------------------------------");
+		System.out.println("총합계\t\t\t\t\\"+100000);
+		System.out.println("---------------------------------------");
+		
+	}
+}
