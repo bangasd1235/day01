2c0dee3b5f6d129ca717379ee6a14b087196239e
bang <asd@asd.com>
2024-09-26 13:08:43 +0900

    엔터 없는 출력

--- src/day01/ExClass01.java
+++ src/day01/ExClass01.java
@@ -4,5 +4,9 @@ public class ExClass01 {
 	public static void main(String[] args) {
 		System.out.println(100); 
 		System.out.println("안녕하세요 "+ 9 );
+		int i = 0;
+		System.out.print("1.안녕");
+		System.out.print("2.안녕");
+		System.out.print("3.안녕");
 	}
 }
