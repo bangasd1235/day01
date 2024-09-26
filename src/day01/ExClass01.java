0f86938867367e45398247dcca9c8fa860c29106
bang <asd@asd.com>
2024-09-26 13:39:36 +0900

    숫자

--- src/day01/ExClass01.java
+++ src/day01/ExClass01.java
@@ -5,8 +5,8 @@ public class ExClass01 {
 		System.out.println(100); 
 		System.out.println("안녕하세요 "+ 9 );
 		int i = 0;
-		System.out.print("1.안녕");
-		System.out.print("2.안녕");
+		System.out.print("1.안녕\n");
+		System.out.print("2.안녕\n");
 		System.out.print("3.안녕");
 	}
 }
